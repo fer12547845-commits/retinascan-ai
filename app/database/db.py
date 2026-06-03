65b39d23d9d323393171460f29f44daf84d60afa
Fernanda Acevedo <fer12547845@gmail.com>
2026-06-03 14:47:18 -0500

    primer commit

---
+++ app/database/db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+
+    host="localhost",
+
+    user="root",
+
+    password="",
+
+    database="retinascan_db"
+
+)
+
+cursor = db.cursor()
