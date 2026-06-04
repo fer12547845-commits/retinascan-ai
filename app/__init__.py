c85a3fd7e31beaa25ae6813f7f263a9a465ec252
Fernanda Acevedo <fer12547845@gmail.com>
2026-06-03 19:09:02 -0500

    fix firebase init

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,7 @@ from flask import Flask
 from app.routes.routes import main
 import firebase_admin
 from firebase_admin import credentials
-import os
-
-from flask import Flask
-from app.routes.routes import main
-import firebase_admin
-from firebase_admin import credentials
+import json
 import os
 
 def create_app():
@@ -17,9 +12,7 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
 
     if not firebase_admin._apps:
-        cred = credentials.Certificate(
-            os.path.join(os.path.dirname(__file__), '..', 'serviceAccountKey.json')
-        )
+        cred = credentials.Certificate(json.loads(os.environ.get("FIREBASE_CREDENTIALS")))
         firebase_admin.initialize_app(cred)
 
     app.register_blueprint(main)
